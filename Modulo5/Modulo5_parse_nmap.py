4b9c8ee6a9d31eb119634aeaee5c63134243055c
BraXploiter <81781383+MasterAdolf@users.noreply.github.com>
2022-07-19 09:40:06 -0300

    Alterando script trocado

--- Modulo5/Modulo5_parse_nmap.py
+++ Modulo5/Modulo5_parse_nmap.py
@@ -15,23 +15,31 @@ from time import strftime
 from pathlib import Path
 
 target = sys.argv[1]
-domain = sys.argv[2]
 headers = {'Accept' : 'application/json', 'Content-Type' : 'application/json'}
-url = 'https://localhost:9200/'+target+'-subdomain/_doc?refresh'
+url = 'https://localhost:9200/'+target+'-portscan/_doc?refresh'
+url_get = 'https://localhost:9200/'+target+'-subdomain/_search'
 auth=('admin', '83d875fc-8789-11ec-9757-00505642c2bf')
-hora = strftime("%Y-%m-%dT%H:%M:%S%Z")
+hora = strftime("%Y-%m-%dT%H:%M:%S")
 scanner = 'nmap'
 dic_ports = {}
 x = str(uuid.uuid1()).split('-')[0]
 container_name = target+'-'+x+'-nmap'
 saida = 'nmap-'+x+'.xml'
-ip = '37.59.174.225'
+ip = sys.argv[2]
 
 def executa():
-    subprocess.check_output('docker run --rm --name '+container_name+' -v /docker/data/'+target+'/temp:/data kali-tools:2.0 nmap -sSV -Pn '+ip+' -oX /data/temp/'+saida+' || true', shell=True)
+    subprocess.check_output('docker run --rm --name '+container_name+' -v /docker/data/'+target+'/temp:/data kali-tools:2.0 nmap -sSV -Pn '+ip+' -oX /data/'+saida+' || true', shell=True)
+
+def consulta(ip):
+	data = {"size":10000}
+	get_doc = requests.get(url_get, headers=headers, auth=auth, data=json.dumps(data), verify=False)
+	parse_scan = json.loads(get_doc.text)
+	for x in parse_scan['hits']['hits']:
+		if(str(x['_source']['server.ip']) == str(ip)):
+			return((str(x['_source']['server.ipblock'])))
 
 def parse():
-    tree = ET.parse('/docker/data/'+target+'/temp/'+x)
+    tree = ET.parse('/docker/data/'+target+'/temp/'+saida)
     root = tree.getroot()
     for i in root.iter('nmaprun'):
         for nmaprun in i:
@@ -40,33 +48,45 @@ def parse():
                     if(host.tag == 'address'):
                         if(':' not in host.attrib['addr']):
                             dic_ports['ip_v4'] = host.attrib['addr']
-                            dic_ports['addrtype'] = host.attrib['addrtype']
+                            dic_ports['network.type'] = host.attrib['addrtype']
                     if(host.tag == 'ports'):
                         for port in host:
                             if(port.tag == 'port'):
-                                dic_ports['protocol'] = port.attrib['protocol']
-                                dic_ports['portid'] = port.attrib['portid']
+                                dic_ports['network.transport'] = port.attrib['protocol']
+                                dic_ports['server.port'] = port.attrib['portid']
                                 for itens in port:
                                     if(itens.tag == 'state'):
-                                        dic_ports['state'] = itens.attrib['state']
+                                        dic_ports['service.state'] = itens.attrib['state']
                                     if(itens.tag == 'service'):
                                         try:
-                                            dic_ports['name'] = itens.attrib['name']
+                                            dic_ports['network.protocol'] = itens.attrib['name']
                                         except:
-                                            dic_ports['name'] = ''
+                                            dic_ports['network.protocol'] = ''
                                         try:
-                                            dic_ports['version'] = itens.attrib['version']
+                                            dic_ports['application.version.number'] = itens.attrib['version']
                                         except:
-                                            dic_ports['version'] = ''
+                                            dic_ports['application.version.number'] = ''
                                         try:
-                                            dic_ports['product'] = itens.attrib['product']
+                                            dic_ports['service.name'] = itens.attrib['product']
                                         except:
-                                            dic_ports['product'] = ''
-                                        try:
-                                            dic_ports['cpe'] = itens.attrib['cpe']
-                                        except:
-                                            dic_ports['cpe'] = ''
-                                        print(dic_ports)
+                                            dic_ports['service.name'] = ''
+                                        dic_ports['server.ipblock'] = consulta(ip)
+                                        data = {
+                    				            '@timestamp':hora,
+                    				            'server.address':ip,
+                    				            'network.protocol':dic_ports['network.protocol'],
+                    				            'server.ip':ip,
+                    				            'server.port':dic_ports['server.port'],
+                    				            'server.ipblock':dic_ports['server.ipblock'],
+                    				            'server.name':dic_ports['service.name'],
+                    				            'server.state':dic_ports['service.state'],
+                    				            'network.transport':dic_ports['network.transport'],
+                    				            'network.type':dic_ports['network.type'],
+                    				            'application.version.number':dic_ports['application.version.number'],
+                    				            'vulnerability.scanner.vendor':scanner
+            				                    }
+                                        r = requests.post(url, headers=headers, auth=auth, data=json.dumps(data), verify=False)
+                                        print (r.text)
 def main():
     executa()
     parse()
